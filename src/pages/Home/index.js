c9e73da992181fde68cdeec80a4cd322dc1324d2
Marco Antonio <marco.oliveira92@hotmail.com>
2019-12-05 13:38:00 -0300

    test

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,85 +1,61 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { MdAddShoppingCart } from 'react-icons/md';
-import Loader from 'react-loader-spinner';
-import { formatPrice } from '../../util/format';
-import api from '../../services/api';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { MdAddShoppingCart } from 'react-icons/md'
+import { formatPrice } from '../../util/format'
+import api from '../../service/api'
 
-import * as CartActions from '../../store/modules/cart/actions';
+import * as CartActions from '../../store/modules/cart/actions'
 
-import { ProductList, Loading } from './styles';
+import { ProductList } from './styles';
 
-class Home extends Component {
-  state = {
-    products: [],
-    loading: true,
-    didMount: false,
-  };
 
-  async componentDidMount() {
-    const response = await api.get('products');
 
+const  Home = (props) => {
+  const [dados, setDados] = useState([])
+
+  useEffect(() => {
+    getAPi()
+  }, [])
+
+ const getAPi = async () => {
+    const response = await api.get('products')
     const data = response.data.map(product => ({
       ...product,
-      priceFormatted: formatPrice(product.price),
-    }));
-
-    setTimeout(() => {
-      this.setState({ products: data, loading: false });
-
-      setTimeout(() => {
-        this.setState({ didMount: true });
-      }, 0);
-    }, 1000);
+      priceFormatted: formatPrice(product.price)
+    }))
+    setDados(data)
   }
 
-  handleAddProduct = id => {
-    const { addToCartRequest } = this.props;
+  const handleAddProduct = (id) => {
+    const { addToCartRequest } = props
 
-    addToCartRequest(id);
-  };
-
-  render() {
-    const { products, loading, didMount } = this.state;
-    const { amount, addingIds } = this.props;
-
-    if (loading) {
-      return (
-        <Loading>
-          <Loader type="MutatingDot" color="#FFFFFF" />
-        </Loading>
-      );
-    }
-    return (
-      <ProductList didMount={didMount ? 1 : 0}>
-        {products.map(product => (
-          <li key={product.id}>
-            <img src={product.image} alt={product.title} />
-            <strong>{product.title}</strong>
-            <span>{product.priceFormatted}</span>
-
-            <button
-              type="button"
-              onClick={() => this.handleAddProduct(product.id)}
-              disabled={addingIds.includes(product.id)}
-            >
-              <div>
-                <MdAddShoppingCart size={16} color="#FFF" />{' '}
-                {amount[product.id] || 0}
-                {addingIds.includes(product.id) && (
-                  <div className="loading">
-                    <Loader type="Oval" color="#FFF" width={18} height={18} />
-                  </div>
-                )}
-              </div>
-              <span>ADD TO CART</span>
-            </button>
-          </li>
-        ))}
-      </ProductList>
-    );
+    addToCartRequest(id)
   }
+
+  return (
+    <ProductList>
+
+    { dados.map(product => (
+      <li key={product.id}>
+        <img src={product.image} alt={product.title} />
+        <strong>{product.title}</strong>
+        <span>{product.priceFormatted}</span>
+        <button type='button' onClick={() => handleAddProduct(product.id)}>
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />{' '}
+            {props.amount[product.id] || 0}
+          </div>
+
+          <span>Adicionar ao Carriho</span>
+        </button>
+      </li>
+
+    ))}
+
+
+    </ProductList>
+  );
 }
 
 const mapStateToProps = state => ({
@@ -87,13 +63,11 @@ const mapStateToProps = state => ({
     amount[product.id] = product.amount;
     return amount;
   }, {}),
-  addingIds: state.cart.addingIds,
+  cart: state.cart.products
 });
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Home);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
